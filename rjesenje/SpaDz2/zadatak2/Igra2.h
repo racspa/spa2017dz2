5db3eb468f15909139022a2dd0ab11708d302dc4
KarloFab <fabijanickarlo1@gmail.com>
2017-06-21 20:45:39 +0200

    Add files via upload

---
+++ rjesenje/SpaDz2/zadatak2/Igra2.h
@@ -0,0 +1,36 @@
+#ifndef IGRA2_H_
+#define IGRA2_H_
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <conio.h>
+#include <ctime>
+#include <Windows.h>
+using namespace std;
+
+class Igra2 {
+private:
+	static const unsigned int STUPACA = 30;
+	static const unsigned int REDAKA =25;
+	int Ax = 12;
+	int Ay = 15;
+	int Vockicax;
+	int Vockicay;
+	char znak;
+	int random(int min,int max);
+	char ploca[REDAKA][STUPACA];
+	bool Vockica();
+	bool pogoden(int ax, int ay, int VockicaX, int VockicaY);
+	char get_user_input();
+	int pomakniAxGore(int &Ax,int &Ay);
+	int pomakniAyDesno(int &Ax, int &Ay);
+	int pomakniAxDolje(int &Ax, int &Ay);
+	int pomakniAyLijevo(int &Ax, int &Ay);
+	void igraj(char znak);
+
+public:
+	Igra2();
+
+};
+
+#endif
